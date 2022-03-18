27c0ac7feb1e10d62ae573ceae11ec1e54b7fb6c
mdexswap <mdexswap@protonmail.com>
2022-03-18 21:56:57 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2e00910cb7fac04ba775bbb5a085c1e.js"
+  "/precache-manifest.f8b732828575abdef7ee7cc2c5718f69.js"
 );
 
 self.addEventListener('message', (event) => {
